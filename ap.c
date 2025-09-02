ab1ff0e899896834231d49c3519bee8238cca36f
dipak921 <dipakchande921@gmail.com>
2025-09-02 12:23:01 +0530

    while_loop

--- ap.c
+++ ap.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 int main(){
     
-    int a = 100;
-    for(int i = 1; a>0; i++){
-        printf("%d ",a);
-        a = a-3;
+    
+    for(int i = 5; i>=0; i--){
+    for(int i = 5; i%2==0; i++){
+        printf("%d \n",i);
+        // a = a-3;
     }
 
     return 0;
---
+++ even.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main(){
+    // int sum=0;
+    // for(int i=0; i<=5; i++)
+    // {
+    //    // printf("%d\n",i);  
+    //     sum = sum + i;  
+    // }
+    // printf(" All sum of = %d",sum);
+
+    for(int i=0; i<=10; i++)
+    {
+        printf("%d\n",i);  
+        i = i +1;
+         
+    }
+
+
+
+
+
+
+    // printf("odd number\n");
+    // // odd number
+    // for(int j= 1; j<=10; j+=2){
+    //     printf("%d\n",j);
+    // }
+
+    // {
+    //     if(!(i%2==0)){ // ! use this sign and print odd number
+    //         printf("%d\n",i);
+    //     }else{
+    //         printf("");  
+    //     } 
+    // }
+    return 0;
+
+}
