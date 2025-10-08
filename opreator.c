d9fbfd6b93d6a37eccb9e2bb6c19c779e6a2ded7
dipak921 <dipakchande921@gmail.com>
2025-10-08 13:52:40 +0530

    oprators

---
+++ opreator.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+int main()
+{
+    // int n1, n2,n3;
+    // printf("Enter a n1 :");
+    // scanf("%d",&n1);
+    // printf("Enter a n2 :");
+    // scanf("%d",&n2);
+
+    // printf("The Additon is : %d",n1 + n2);
+    // printf("\nThe Substraction is : %d",n1 - n2);
+    // printf("\nThe Multiplication is : %d",n1 * n2);
+    // printf("\nThe Division is : %d",n1 / n2);
+    // printf("\nThe Remenderz is : %d",n1 % n2);
+
+//Assignment Operator = 
+    // int n1 = 2,n2 = 5,n3;
+    // n3 = n2/2 *n1;
+    // printf("%d",n3);
+
+// Short hand 
+    // int a =  10, b = 5;
+    // a+=b; // a = a + b;
+    // a-=b; // a = a - b;
+    // a*=b; // a = a * b;
+    // a/=b; // a = a / b;
+    // a%=b; // a = a % b;
+
+    // printf("%d",a);
+
+// Relational  Operator / Comparison Operator
+    // int a = 20 ,b = 3;
+    // printf("%d\n", a>b);  //1
+    // printf("%d\n", a<b);  // 0
+    // printf("%d\n", a>=b);  //1
+    // printf("%d\n", a<=b);  //0
+    // printf("%d\n", a==b);  //0
+    // printf("%d\n", a!=b);  //1
+
+
+
+// Logical Opreator 
+ //& || !
+
+    int a = 20, b = 10 ,c;
+    //c = a<b && b<50; // 0
+    //c = a<b || b<50; // 1
+   // c = !(a<b || b<50);
+
+//    c = a>b && 0;
+    // printf("%d",!a);
+
+    int x  = 10, y = 20;
+    c = x > y && x+=10;
+    printf("c = %d and x = %d",c,a);
+
+
+    return 0;
+}
